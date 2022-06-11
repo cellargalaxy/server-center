968ec6a672f5864f888ecdd2e8274138b6a2826d
cellargalaxy <cellargalaxy@gmail.com>
2022-06-11 15:26:46 +0800

    server_name

--- sdk/sdk.go
+++ sdk/sdk.go
@@ -155,14 +155,14 @@ func (this *ServerCenterClient) GetAndParseLastServerConf(ctx context.Context) (
 		return nil, err
 	}
 	if object == nil {
-		logrus.WithContext(ctx).WithFields(logrus.Fields{"current_version": this.conf.Version}).Info("查询并解析最新服务配置，服务配置无更新")
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"server_name": this.conf.ServerName, "current_version": this.conf.Version}).Info("查询并解析最新服务配置，服务配置无更新")
 		return nil, nil
 	}
 	if object.Version <= this.conf.Version {
-		logrus.WithContext(ctx).WithFields(logrus.Fields{"current_version": this.conf.Version, "last_version": object.Version}).Warn("查询并解析最新服务配置，服务配置无更新")
+		logrus.WithContext(ctx).WithFields(logrus.Fields{"server_name": this.conf.ServerName, "current_version": this.conf.Version, "last_version": object.Version}).Warn("查询并解析最新服务配置，服务配置无更新")
 		return nil, nil
 	}
-	logrus.WithContext(ctx).WithFields(logrus.Fields{"current_version": this.conf.Version, "last_version": object.Version}).Info("查询并解析最新服务配置，服务配置更新")
+	logrus.WithContext(ctx).WithFields(logrus.Fields{"server_name": this.conf.ServerName, "current_version": this.conf.Version, "last_version": object.Version}).Info("查询并解析最新服务配置，服务配置更新")
 	err = this.handler.ParseConf(ctx, *object)
 	if err == nil {
 		this.conf = *object
