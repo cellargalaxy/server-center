72839a81a7b9008f382b40981581f4ee9cf8cbcb
cellargalaxy <cellargalaxy@gmail.com>
2022-06-18 17:44:04 +0800

    备份

--- install-docker.sh
+++ install-docker.sh
@@ -7,13 +7,6 @@ if [ -z $server_name ]; then
   server_name="server_center"
 fi
 
-while :; do
-  if [ ! -z $mysql_dsn ]; then
-    break
-  fi
-  read -p "please enter mysql_dsn(required):" mysql_dsn
-done
-
 if [ -z $listen_port ]; then
   read -p "please enter listen port(default:7557):" listen_port
 fi
@@ -23,7 +16,6 @@ fi
 
 echo
 echo 'server_name: '$server_name
-echo 'mysql_dsn: '$mysql_dsn
 echo 'listen_port: '$listen_port
 echo 'input any key go on, or control+c over'
 read
@@ -48,7 +40,6 @@ docker run -d \
   -v $server_name'_resource':/resource \
   -p $listen_port:7557 \
   -e server_name=$server_name \
-  -e mysql_dsn=$mysql_dsn \
   $server_name
 
 echo 'all finish'
