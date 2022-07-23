2aa7f5a2244113ed2c7dd4a584f1eb11486694ef
cellargalaxy <cellargalaxy@gmail.com>
2022-07-23 15:30:11 +0800

    备份

--- install-docker.sh
+++ install-docker.sh
@@ -7,6 +7,20 @@ if [ -z $server_name ]; then
   server_name="server_center"
 fi
 
+if [ -z $server_center_address ]; then
+  read -p "please enter server_center_address(default:''):" server_center_address
+fi
+if [ -z $server_center_address ]; then
+  server_center_address=""
+fi
+
+if [ -z $server_center_secret ]; then
+  read -p "please enter server_center_secret(default:''):" server_center_secret
+fi
+if [ -z $server_center_secret ]; then
+  server_center_secret=""
+fi
+
 if [ -z $listen_port ]; then
   read -p "please enter listen port(default:7557):" listen_port
 fi
@@ -16,6 +30,8 @@ fi
 
 echo
 echo 'server_name: '$server_name
+echo "server_center_address: $server_center_address"
+echo "server_center_secret: $server_center_secret"
 echo 'listen_port: '$listen_port
 echo 'input any key go on, or control+c over'
 read
@@ -40,6 +56,8 @@ docker run -d \
   -v $server_name'_resource':/resource \
   -p $listen_port:7557 \
   -e server_name=$server_name \
+  -e server_center_address=$server_center_address \
+  -e server_center_secret=$server_center_secret \
   $server_name
 
 echo 'all finish'
